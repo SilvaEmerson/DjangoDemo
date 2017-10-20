9c2b63a9a610c9ccd5ae97bc1fb6ad84391f45f0
SilvaEmerson <emersonjose877@gmail.com>
2017-10-20 17:48:51 -0300

    initial commit

---
+++ restaurantes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Restaurant(models.Model):
+	"""docstring for Restaurant"""
+	owner = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
+	speciality = models.CharField(max_length=200)
+	fund_date = models.DateField(
+		blank=True, null=True)
+
+	def __str__(self):
+		return self.name
