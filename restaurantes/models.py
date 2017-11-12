47e7669207b20300483b5d464351b292727d0da1
SilvaEmerson <emersonjose877@gmail.com>
2017-11-12 13:17:03 -0300

    Fix footer, edit form_create_restaurant.html

--- restaurantes/models.py
+++ restaurantes/models.py
@@ -3,14 +3,14 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Restaurant(models.Model):
-	"""docstring for Restaurant"""
-	owner = models.CharField(max_length=200)
-	name = models.CharField(max_length=200)
-	adress = models.CharField(max_length=200)
-	speciality = models.CharField(max_length=200)
-	fund_date = models.DateField(
-		blank=True, null=True)
 
-	def __str__(self):
-		return self.name
+class Restaurant(models.Model):
+    owner = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    adress = models.CharField(max_length=200)
+    speciality = models.CharField(max_length=200)
+    fund_date = models.DateField(
+        blank=True, null=True)
+
+    def __str__(self):
+        return self.name
