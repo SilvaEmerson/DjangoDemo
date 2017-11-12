c37f3e0aebb2d48a25ef1781452d098e396973cb
SilvaEmerson <emersonjose877@gmail.com>
2017-11-12 13:15:40 -0300

    Add edit option

--- restaurantes/urls.py
+++ restaurantes/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.rests_list, name='rests_list'),
-	url(r'^new/$', views.restaurant_new, name='restaurant_new')
+    url(r'^$', views.rests_list, name='rests_list'),
+    url(r'^new/$', views.restaurant_new, name='restaurant_new'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.restaurant_edit,
+        name='restaurant_edit'),
+    url(r'^del/(?P<pk>[0-9]+)/$', views.restaurant_delete,
+        name='restaurant_delete')
 ]
--- restaurantes/views.py
+++ restaurantes/views.py
@@ -1,29 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Restaurant
 from .forms import RestaurantNew
-from django.shortcuts import redirect
 # Create your views here.
 
+
 def rests_list(request):
-	restaurants = Restaurant.objects.all()
-	return render(request, 'restaurantes/rests_list.html',
-			{'restaurants': restaurants})
+    restaurants = Restaurant.objects.order_by('name')
+    return render(request, 'restaurantes/rests_list.html',
+                  {'restaurants': restaurants})
+
 
 def restaurant_new(request):
-	if request.method == "POST":
-		form = RestaurantNew(request.POST)
-		if form.is_valid():
-			restaurant = form.save(commit=False)
-			# restaurant.name = request.name
-			# restaurant.owner = request.owner
-			# restaurant.adress = request.adress
-			# restaurant.fund_date = request.fund_date
-			# restaurant.speciality = request.speciality
-			restaurant.save()
+    if request.method == "POST":
+        form = RestaurantNew(request.POST)
+        if form.is_valid():
+            restaurant = form.save(commit=False)
+            restaurant.save()
+            return redirect('rests_list')
+    else:
+        form = RestaurantNew()
 
-			return redirect('rests_list')
-	else:
-		form = RestaurantNew()
+    return render(request, 'restaurantes/form_create_restaurant.html',
+                  {'form': form})
 
-	return render(request, 'restaurantes/form_create_restaurant.html',
-		{'form': form})
+
+def restaurant_edit(request, pk):
+    restaurant = get_object_or_404(Restaurant, pk=pk)
+    if request.method == "POST":
+        form = RestaurantNew(request.POST, instance=restaurant)
+        print(form)
+        if form.is_valid():
+            restaurant = form.save(commit=False)
+            restaurant.save()
+            return redirect('rests_list')
+    else:
+        form = RestaurantNew(instance=restaurant)
+
+    return render(request, 'restaurantes/form_edit_restaurant.html',
+                  {'form': form, 'restaurant': restaurant})
+
+
+def restaurant_delete(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    restaurant.delete()
+    return redirect('rests_list')
