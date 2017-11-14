81559675a84e30258c96e42d3de75410ff6d550a
SilvaEmerson <emersonjose877@gmail.com>
2017-11-13 23:11:55 -0300

    Add the satisfactions icons

--- restaurantes/views.py
+++ restaurantes/views.py
@@ -4,11 +4,24 @@ from .models import Restaurant
 from .forms import RestaurantNew
 # Create your views here.
 
+satisfations = {'sentiment_very_dissatisfied': 'vote0',
+                'sentiment_dissatisfied': 'vote1',
+                'sentiment_neutral': 'vote2', 'sentiment_satisfied': 'vote3',
+                'sentiment_very_satisfied': 'vote4'}
+
 
 def rests_list(request):
+    global satisfations
+    satisfations_icons = list(satisfations.keys())
+    urls = list(satisfations.values())
+    urls.sort()
+    numbers = [i for i in range(5)]
     restaurants = Restaurant.objects.order_by('name')
     return render(request, 'restaurantes/rests_list.html',
-                  {'restaurants': restaurants})
+                  {'restaurants': restaurants,
+                   'satisfations': satisfations,
+                   'urls': urls,
+                   'numbers': numbers})
 
 
 def restaurant_new(request):
@@ -44,4 +57,38 @@ def restaurant_edit(request, pk):
 def restaurant_delete(request, pk):
     restaurant = Restaurant.objects.get(pk=pk)
     restaurant.delete()
+    return redirect('rests_list')
+
+
+def vote0(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    restaurant.votes += 1
+    return redirect('rests_list')
+
+
+def vote1(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    restaurant.votes += 1
+    restaurant.total_points += 1
+    return redirect('rests_list')
+
+
+def vote2(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    restaurant.votes += 1
+    restaurant.total_points += 2
+    return redirect('rests_list')
+
+
+def vote3(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    restaurant.votes += 1
+    restaurant.total_points += 3
+    return redirect('rests_list')
+
+
+def vote4(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    restaurant.votes += 1
+    restaurant.total_points += 4
     return redirect('rests_list')
