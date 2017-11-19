c840965e8b065fde6e50c1ab547dfaca38afacaa
SilvaEmerson <emersonjose877@gmail.com>
2017-11-18 23:28:05 -0300

    Edit models.py

--- restaurantes/models.py
+++ restaurantes/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from django.utils import timezone
-
 # Create your models here.
+
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=1000, null=True)
+
+    def __str__(self):
+        return self.comment
 
 
 class Restaurant(models.Model):
@@ -11,6 +16,7 @@ class Restaurant(models.Model):
     speciality = models.CharField(max_length=200)
     fund_date = models.DateField(
         blank=True, null=True)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
