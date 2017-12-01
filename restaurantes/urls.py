038d01586ce03f10158c4a2643280038090a0071
SilvaEmerson <emersonjose877@gmail.com>
2017-12-01 19:35:18 -0300

    Edit rests_list.html

--- restaurantes/urls.py
+++ restaurantes/urls.py
@@ -9,10 +9,5 @@ urlpatterns = [
     url(r'^del/(?P<pk>[0-9]+)/$', views.restaurant_delete,
         name='restaurant_delete'),
     url(r'^addfeed/(?P<pk>[0-9]+)/$', views.do_feedback,
-        name='add_feedback'),
-    url(r'^vote0/(?P<pk>[0-9]+)/$', views.vote0, name='vote0'),
-    url(r'^vote1/(?P<pk>[0-9]+)/$', views.vote1, name='vote1'),
-    url(r'^vote2/(?P<pk>[0-9]+)/$', views.vote2, name='vote2'),
-    url(r'^vote3/(?P<pk>[0-9]+)/$', views.vote3, name='vote3'),
-    url(r'^vote4/(?P<pk>[0-9]+)/$', views.vote4, name='vote4')
-]
+        name='add_feedback')
+    ]
